a4cfbf1db4e7ccb08f8a1e999697375f4996e52d
Haikel Fazzani <haikel.fazzanii@outlook.fr>
2018-02-23 14:45:55 +0100

    Update client.c

--- client.c
+++ client.c
@@ -2,35 +2,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+
 int main(int argc , char *argv[]) {
 
-	char buffer[BUFSIZ];
 	struct sockaddr_in addr;
+	struct hostent *htpr;
+	char buffer[BUFSIZ];
 
-	int s = socket(AF_INET, SOCK_STREAM,0);
+	int s = socket(AF_INET , SOCK_STREAM , 0);
 
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(argv[1]));
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(atoi(argv[2]));
+	htpr = gethostbyname(argv[1]);
+	memcpy(&addr.sin_addr, htpr->h_addr , htpr->h_length);
 
-	connect(s , (struct sockaddr *)&addr ,sizeof(addr));
+	connect(s , (struct sockaddr *)&addr , sizeof(addr));
 
-	//char str[100];
-    while(1) {
-    	read(0,buffer , BUFSIZ);
-    	if(strcmp(buffer , "quit"))
-    		break;
-    }
-
-	write(s , buffer , strlen(buffer));
-	read(s , buffer , sizeof(buffer));
-	printf("%s\n", buffer);
-	
+	while(1) {
+		
+		strcpy(buffer , 0);
+		write(s , buffer , strlen(buffer));
+		fflush(stdin);
+		read( s , buffer , BUFSIZ);
+		//printf("%s\n", buffer);
+	}
 	close(s);
 }
